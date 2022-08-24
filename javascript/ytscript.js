0f895fb0bf8493938bb7577c66b07fafa1ef0ffd
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-08-23 21:28:29 -0600

    how to embed an iframe

--- javascript/ytscript.js
+++ javascript/ytscript.js
@@ -1,9 +1,11 @@
-//var apikey=AIzaSyA5PA77ib11vqf_lRxL0Jl9iT6hANK59hI
+var apikey= "AIzaSyA5PA77ib11vqf_lRxL0Jl9iT6hANK59hI"
 var execute = document.getElementById('execute')
 
-function getApi() {
-  var requestUrl= 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=AIzaSyA5PA77ib11vqf_lRxL0Jl9iT6hANK59hI';
 
+
+/*function getApi() {
+  var requestUrl= 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=AIzaSyA5PA77ib11vqf_lRxL0Jl9iT6hANK59hI';
+  var response = "";
   fetch(requestUrl)
   .then(function(response) {
     return response.json();
@@ -18,11 +20,31 @@ function getApi() {
 }
 execute.addEventListener('click', getApi);
 
-function searchByKeyword() {
-  var results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
+ /**
+   * Sample JavaScript code for youtube.search.list
+   * See instructions for running APIs Explorer code samples locally:
+   * https://developers.google.com/explorer-help/code-samples#javascript
+   */
 
-  for(var i in results.items) {
-    var item = results.items[i];
-    Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
+  function loadClient() {
+    gapi.client.setApiKey("AIzaSyA5PA77ib11vqf_lRxL0Jl9iT6hANK59hI");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
   }
-}
+  // Make sure the client is loaded before calling this method.
+  function execute() {
+    return gapi.client.youtube.search.list({
+      "part": [
+        "snippet"
+      ],
+      "maxResults": 1
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  gapi.load("client");
+  loadClient
