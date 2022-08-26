01040d5e1609cecba49b4f67401591f4e1515c09
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2022-08-25 20:02:30 -0600

    append default yt list to page

--- javascript/ytscript.js
+++ javascript/ytscript.js
@@ -1,10 +1,44 @@
 var apikey= "AIzaSyA5PA77ib11vqf_lRxL0Jl9iT6hANK59hI"
 var execute = document.getElementById('execute')
+var setApiKey= ("");
+var searchArtist=("");
+var button= document.querySelector(".button");
+var videosEl=document.getElementById("videos")
+ 
 
+ /**
+   * Sample JavaScript code for youtube.search.list
+   * See instructions for running APIs Explorer code samples locally:
+   * https://developers.google.com/explorer-help/code-samples#javascript
+   */
 
+  
+  /*function loadClient() {
+    setApiKey="";
 
-/*function getApi() {
-  var requestUrl= 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=AIzaSyA5PA77ib11vqf_lRxL0Jl9iT6hANK59hI';
+    gapi.client.setApiKey("YAIzaSyA5PA77ib11vqf_lRxL0Jl9iT6hANK59hI");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute() {
+    return gapi.client.youtube.search.list({
+      "part": [
+        "snippet"
+      ],
+      "maxResults": 5,
+      "q": "surfing"
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  };*/
+
+function getApi() {
+  var requestUrl= 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=' + searchArtist + 'mostPopular&regionCode=US&key=AIzaSyA5PA77ib11vqf_lRxL0Jl9iT6hANK59hI';
   var response = "";
   fetch(requestUrl)
   .then(function(response) {
@@ -12,11 +46,16 @@ var execute = document.getElementById('execute')
   })
   .then(function(data) {
     console.log(data);
-    for (var i =0; i <data.length; i++) {
-      var videos1 = document.createElement('p');
-      videos1.textContent = data[i].title;
+    console.log(data.items.length)
+    for (var i =0; i <data.items.length; i++) {
+      console.log(i)
+      var videos1 = document.createElement('li');
+      videos1.textContent = data.items[i].snippet.title;
+      console.log(videos1)
+      console.log(videosEl)
+      videosEl.appendChild(videos1)
     }
-  });
+  });console.log(response)
 }
 execute.addEventListener('click', getApi);
 
@@ -26,7 +65,7 @@ execute.addEventListener('click', getApi);
    * https://developers.google.com/explorer-help/code-samples#javascript
    */
 
-  function loadClient() {
+  /*function loadClient() {
     gapi.client.setApiKey("AIzaSyA5PA77ib11vqf_lRxL0Jl9iT6hANK59hI");
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(function() { console.log("GAPI client loaded for API"); },
@@ -47,4 +86,4 @@ execute.addEventListener('click', getApi);
               function(err) { console.error("Execute error", err); });
   }
   gapi.load("client");
-  loadClient
+  loadClient*/
