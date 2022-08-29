f84f34a20fadbfd38e5228d729d294edbe028148
Ayoub <biliad8786@gmail.com>
2022-08-29 18:37:53 -0400

    You can search an artists name and it will show up in the artist tag

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,5 @@
-//Variables 
+var buttonEl = document.getElementById("searchbutton");
+console.log(buttonEl)
 var button= document.querySelector(".button");
 var searchedArtist = document.querySelector("#searchedArtist");
 var msgDiv = document.querySelector("#msg");
@@ -7,7 +8,6 @@ var msgDiv = document.querySelector("#msg");
 button.addEventListener("click", function(event){
 event.preventDefault();
 
-//saves serched artist to local storage
 var artist = document.querySelector(".artist").value;
 
   localStorage.setItem("artist", artist);
@@ -15,7 +15,6 @@ var artist = document.querySelector(".artist").value;
 displayLastSearch();
 });
 
-//Displays last serched artist name
 function displayLastSearch() {
     var artist = localStorage.getItem("artist");
 
@@ -23,3 +22,39 @@ function displayLastSearch() {
     console.log(artist);
 }
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b0a5799d28msh34be151581c8ca3p122bbejsn6bc36006be7a',
+		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+	}
+};
+function apicall(event) {
+    event.preventDefault()
+    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${search}`, options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+}
+var getData = () =>{
+	var userInput = input.value
+	console.log(userInput)
+	
+}
+function fetchArtist(search){
+	let artistUrl=`https://deezerdevs-deezer.p.rapidapi.com/search?q=${search}`
+	fetch(artistUrl)
+	.then(function(res){
+		return res.json()
+
+
+	}).then (function (data){
+		if(!data){
+			alert("no artist found")
+		}else{
+			console.log(data)
+		}
+	})
+}
+buttonEl.addEventListener('click', apicall)
