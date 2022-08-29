16056468c6d4a9d0881636d134c756954eb6ce0c
jyp1763 <108202344+jyp1763@users.noreply.github.com>
2022-08-28 22:22:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var buttonEl = document.getElementById("searchbutton");
+console.log(buttonEl)
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b0a5799d28msh34be151581c8ca3p122bbejsn6bc36006be7a',
+		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+	}
+};
+function apicall(event) {
+    event.preventDefault()
+    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${search}`, options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+}
+var getData = () =>{
+	var userInput = input.value
+	console.log(userInput)
+	
+}
+function fetchArtist(search){
+	let artistUrl=`https://deezerdevs-deezer.p.rapidapi.com/search?q=${search}`
+	fetch(artistUrl)
+	.then(function(res){
+		return res.json()
+
+
+	}).then (function (data){
+		if(!data){
+			alert("no artist found")
+		}else{
+			console.log(data)
+		}
+	})
+}
+buttonEl.addEventListener('click', apicall)
